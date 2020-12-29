429e1de18d1d41b5fcb75a0819841af3b1b5f024
Daterdum <60192984+Daterdum@users.noreply.github.com>
2020-12-30 00:40:37 +0700

    Route finder, le finale

---
+++ Route_finder/algorithm.py
@@ -0,0 +1,172 @@
+from math import inf
+
+
+def shortest_route(coordinates, obstacles, size=500):
+    """
+    Function for finding shortest route between 2 points
+    :param coordinates: dictionary {'s_x': start_x, 's_y': start_y, 'e_x': end_x, 'e_y': end_y, 'size': size}
+    :param obstacles: dictionary with all obstacles {№: {'x': x, 'y': y, 'r': r}, ...}
+    :param size: size of canvas
+    :return:
+    """
+    canvas_ar = [[]]
+    step = int(coordinates['size'])
+
+    def collides(obstacle, node):
+        """
+        Function for checking collision for each node in array
+        """
+        if node.x in range(obstacle['x'] - obstacle['r'], obstacle['x'] + obstacle['r']):
+            if node.y in range(obstacle['y'] - obstacle['r'], obstacle['y'] + obstacle['r']):
+                return True
+        return False
+
+    def canvas_to_arr(obstacles, size=500, step=30):
+        """
+        Function for creating array of Nodes, and filling them with correct 'is_empty' property
+        """
+        line = 0
+        element = 0
+        for y in range(0, size, step):
+            canvas_ar.append([])
+            for x in range(0, size, step):
+                canvas_ar[line].append([])
+                check = 0
+                for obstacle in obstacles.values():
+                    check = 1 if collides(obstacle, Node(x, y)) else check
+                canvas_ar[line][element] = Node(x, y, is_empty=True if check == 0 else False)
+                element += 1
+            element = 0
+            line += 1
+
+    def lee_algorithm(array, start, end):
+        """
+        Main algorithm for finding shortest route
+        """
+        array[start[0]][start[1]].marker = 0
+
+        def _indication(array):
+            """
+            Function for marking each Node in array according to its distance from start node.
+            Stops when end Node is marked.
+            """
+            def _filter(variable):
+                if variable is None:
+                    return False
+                else:
+                    return True
+            mark = 0
+            while (array[end[0]][end[1]].marker is None):
+                draw_markers()  # remove this
+                for line in array:
+                    for element in line:
+                        if element.marker == mark:
+                            for neighbour in filter(_filter, element.neighbours.values()):
+                                if neighbour.marker is None and neighbour.is_empty:
+                                    neighbour.marker = mark + 1
+                print('\n mark = {}\n'.format(mark))
+                mark += 1
+
+        route_coordinates = []
+        route_coordinates.append(array[end[1]][end[0]])
+
+        def route_back(point):
+            """
+            Function for building route back from end to start according to markers
+            """
+            if not point.is_empty:
+                pass
+            elif point.marker == 0:
+                pass
+            else:
+                closest_neighbour = point.closest_neighbour()
+                route_coordinates.append(closest_neighbour)
+                route_back(closest_neighbour)
+
+        if array[end[1]][end[0]].marker is None:
+            return [0]
+        else:
+            route_back(array[end[1]][end[0]])
+            return route_coordinates
+
+    def arr_to_graph(array):
+        """
+        Function for creating graph system between Nodes in array
+        """
+        for line_num, line in enumerate(array):
+            for element_num, element in enumerate(line):
+                if element.is_empty:
+                    element.neighbours['top_nbr'] = array[line_num - 1][element_num] if line_num != 0 else None
+                    element.neighbours['bot_nbr'] = array[line_num + 1][element_num] if line_num != len(array) - 1 else None
+                    element.neighbours['left_nbr'] = array[line_num][element_num - 1] if element_num != 0 else None
+                    element.neighbours['right_nbr'] = array[line_num][element_num + 1] if element_num != len(line) - 1 else None
+
+    def draw_markers():
+        """
+        Support function.
+        Used to print markers of each node in array. Isn't used in completing task.
+        """
+        for line in canvas_ar:
+            temp = []
+            for element in line:
+                if element.marker is None:
+                    if not element.is_empty:
+                        temp.append('*')
+                    else:
+                        temp.append('_')
+                else:
+                    temp.append(str(element.marker))
+            print(temp)
+
+    _obstacles = {k: {in_k: int(in_v) for in_k, in_v in v.items()} for k, v in obstacles.items()}
+    canvas_to_arr(obstacles=_obstacles, size=size, step=step)
+    canvas_ar.pop()
+    arr_to_graph(canvas_ar)
+    start_node = [int(int(coordinates['s_x']) / step), int(int(coordinates['s_y']) / step)]
+    end_node = [int(int(coordinates['e_x']) / step), int(int(coordinates['e_y']) / step)]
+    output = lee_algorithm(canvas_ar, start_node, end_node)
+    draw_markers()
+    return output
+
+
+class Node:
+    """
+    Class of objects used in creating graph for building shortest route.
+    Contains information and references about its neighbours.
+    is_empty parameter shows if node contains obstacle or its part.
+    """
+    def __init__(self, x, y, side=30, is_empty=False, top_nbr=None, bot_nbr=None, left_nbr=None, right_nbr=None):
+        self.x = x
+        self.y = y
+        self.side = side  # not implemented yet
+        self.neighbours = {'top_nbr': top_nbr, 'bot_nbr': bot_nbr, 'left_nbr': left_nbr, 'right_nbr': right_nbr}
+        self.is_empty = is_empty
+        self.marker = None
+
+    def x_g(self, step=30):
+        return int(self.x / step) if self.x != 0 else 0
+
+    def y_g(self, step=30):
+        return int(self.y / step) if self.y != 0 else 0
+
+    def marker(self):
+        return self.marker
+
+    def closest_neighbour(self):
+        lowest_marker = inf
+        point = None
+        for neighbour in [x for x in self.neighbours.values() if x is not None]:
+            if neighbour.marker is None:
+                continue
+            elif neighbour.marker < lowest_marker:
+                lowest_marker = neighbour.marker
+                point = neighbour
+        return point
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- Route_finder/gui.py
+++ Route_finder/gui.py
@@ -1,4 +1,87 @@
 import tkinter as tk
+from json import load, dump
+from algorithm import shortest_route
+
+
+class Obstacles_GUI:
+    def __init__(self, master):
+        self.master = master
+        self.obstacle_counter = 0
+        master.title("Input obstacles")
+        with open('obstacles.json', 'w') as f:
+            dump({}, f)
+        self.input_box()
+
+    def input_box(self):
+        title_frame = tk.Frame(self.master, borderwidth=2, relief=tk.FLAT)
+        title_frame.pack(pady=10, fill=tk.X)
+        title_label = tk.Label(title_frame, text='Enter coordinates of obstacles')
+        title_label.pack()
+
+        main_frame = tk.Frame(self.master, borderwidth=2, relief=tk.FLAT)
+        main_frame.pack(fill=tk.BOTH, side=tk.TOP, padx=5)
+        # start frame
+        frame_start = tk.Frame(main_frame, borderwidth=2, relief=tk.RAISED)
+        frame_start.pack(fill=tk.X, side=tk.LEFT, padx=5)
+        label_s = tk.Label(frame_start, text="Press 'Add' to add more obstacle, press")
+        label_s.pack()
+
+        frame_x = tk.Frame(frame_start)
+        frame_x.pack(fill=tk.X, side=tk.LEFT, padx=10, pady=10)
+
+        label_x = tk.Label(frame_x, text="Enter X coordinate", padx=5, pady=5)
+        label_x.pack()
+
+        self.entry_x = tk.Entry(frame_x)
+        self.entry_x.pack()
+
+        frame_y = tk.Frame(frame_start)
+        frame_y.pack(fill=tk.X, side=tk.LEFT, padx=10, pady=10)
+
+        label_y = tk.Label(frame_y, text="Enter Y coordinate")
+        label_y.pack()
+
+        self.entry_y = tk.Entry(frame_y)
+        self.entry_y.pack()
+
+        frame_r = tk.Frame(frame_start)
+        frame_r.pack(fill=tk.X, side=tk.LEFT, padx=10, pady=10)
+
+        label_r = tk.Label(frame_r, text="Enter radius")
+        label_r.pack()
+
+        self.entry_r = tk.Entry(frame_r)
+        self.entry_r.pack()
+
+        # frame for both buttons
+        frame_buttons = tk.Frame(self.master, borderwidth=2, relief=tk.FLAT)
+        frame_buttons.pack(side=tk.TOP, pady=30)
+        # submit button frame
+        frame_submit_btn = tk.Frame(frame_buttons, borderwidth=2, relief=tk.FLAT)
+        frame_submit_btn.pack(side=tk.LEFT, pady=30)
+
+        submit_button = tk.Button(frame_submit_btn, text="Enter", command=self.submit_input, height=3, width=10)
+        submit_button.pack()
+
+        # add button frame
+        frame_add_btn = tk.Frame(frame_buttons, borderwidth=2, relief=tk.FLAT)
+        frame_add_btn.pack(side=tk.LEFT, pady=30)
+
+        add_button = tk.Button(frame_add_btn, text="Add", command=self._add_value, height=3, width=10)
+        add_button.pack()
+
+    def _add_value(self):  # write code here
+        self.obstacle_counter += 1
+        temp = load(open('obstacles.json'))
+        temp.update({self.obstacle_counter: {'x': self.entry_x.get(), 'y': self.entry_y.get(), 'r': self.entry_r.get()}})
+        with open(file='obstacles.json', mode='w') as f:
+            dump(temp, f)
+        self.entry_y.delete(0, 'end')
+        self.entry_x.delete(0, 'end')
+        self.entry_r.delete(0, 'end')
+
+    def submit_input(self):  # needs rework
+        self.master.destroy()
 
 
 class Input_GUI:
@@ -8,8 +91,9 @@ class Input_GUI:
         self.input_box()
 
     def input_box(self):
+        # size frame
         frame_size = tk.Frame(self.master, borderwidth=2, relief=tk.RAISED)
-        frame_size.pack(pady=10)
+        frame_size.pack(pady=5)
 
         label_size = tk.Label(frame_size, text="Enter size")
         label_size.pack()
@@ -17,7 +101,10 @@ class Input_GUI:
         self.entry_size = tk.Entry(frame_size)
         self.entry_size.pack()
 
-        frame_start = tk.Frame(self.master, borderwidth=2, relief=tk.RAISED)
+        frame_coordinates = tk.Frame(self.master, borderwidth=2, relief=tk.FLAT)
+        frame_coordinates.pack(fill=tk.X, side=tk.TOP, padx=5)
+        # start frame
+        frame_start = tk.Frame(frame_coordinates, borderwidth=2, relief=tk.RAISED)
         frame_start.pack(fill=tk.X, side=tk.LEFT, padx=5)
         label_s = tk.Label(frame_start, text="Start point cooridnates")
         label_s.pack()
@@ -31,7 +118,6 @@ class Input_GUI:
         self.entry_x = tk.Entry(frame_x)
         self.entry_x.pack()
 
-
         frame_y = tk.Frame(frame_start)
         frame_y.pack(fill=tk.X, side=tk.LEFT, padx=10, pady=10)
 
@@ -41,11 +127,10 @@ class Input_GUI:
         self.entry_y = tk.Entry(frame_y)
         self.entry_y.pack()
 
-
-
-        frame_end = tk.Frame(self.master, borderwidth=2, relief=tk.RAISED)
+        # end frame
+        frame_end = tk.Frame(frame_coordinates, borderwidth=2, relief=tk.RAISED)
         frame_end.pack(fill=tk.X, side=tk.LEFT, padx=5)
-        label_e = tk.Label(frame_end, text="End point cooridnates")
+        label_e = tk.Label(frame_end, text="End point coordinates")
         label_e.pack()
 
         frame_x_e = tk.Frame(frame_end)
@@ -57,7 +142,6 @@ class Input_GUI:
         self.entry_x_e = tk.Entry(frame_x_e)
         self.entry_x_e.pack()
 
-
         frame_y_e = tk.Frame(frame_end)
         frame_y_e.pack(fill=tk.X, side=tk.LEFT, padx=10, pady=10)
 
@@ -67,56 +151,126 @@ class Input_GUI:
         self.entry_y_e = tk.Entry(frame_y_e)
         self.entry_y_e.pack()
 
+        # submit button frame
+        frame_submit_btn = tk.Frame(self.master, borderwidth=2, relief=tk.FLAT)
+        frame_submit_btn.pack(fill=tk.X, side=tk.TOP, pady=30)
 
-
-        frame_submit_btn = tk.Frame(self.master, borderwidth=2, relief=tk.RAISED)
-        frame_submit_btn.pack(pady=10)
-
-        submit_button = tk.Button(self.master, command=self.submit)
+        submit_button = tk.Button(frame_submit_btn, text="Enter", command=self.submit_input, height=3, width=10)
         submit_button.pack()
 
-    def submit(self):
-        self.start_x = self.entry_x.get()
-        self.start_y = self.entry_y.get()
-        self.end_x = self.entry_x_e.get()
-        self.end_y = self.entry_y_e.get()
-        self.size = self.entry_size.get()
-
-
-
-
+    def submit_input(self):
+        try:
+            with open(file='input.json', mode='w') as f:
+                temp = {
+                        's_x': self.entry_x.get(),
+                        's_y': self.entry_y.get(),
+                        'e_x': self.entry_x_e.get(),
+                        'e_y': self.entry_y_e.get(),
+                        'size': self.entry_size.get()
+                }
+                dump(temp, f)
+            self.master.destroy()
+        except AttributeError:  # Add exception workaround
+            print("Error")
 
 
 class GUI:
+    """
+    General GUI class, builds main window, canvas for showing elements and gives methods for interacting with them
+    """
+
     def __init__(self, master):
         self.master = master
+        self.robot = None
         master.title("mpiaa_rgz_v6")
+        self._create_widgets()
+
+    def _create_widgets(self):  # method for initial building widgets
+        self._build_menu()
         self.frame = tk.Frame(self.master, relief=tk.RAISED, borderwidth=1)
-        self.frame.pack()
-        self.additional_frame = tk.Frame(self.master)
-        self.create_widgets()
+        self.frame.pack(side=tk.LEFT)
+        self._canvas_init()
+        self.r_frame = tk.Frame(self.master, relief=tk.RAISED, borderwidth=1)
+        self.r_frame.pack(side=tk.RIGHT)
+        self._do()
 
-    def create_widgets(self):
-        label = tk.Label(self.master, text="hello there")
-        label.pack()
-        self.build_menu()
-
-    def build_menu(self):
+    def _build_menu(self):  # needs extension on save and upload scenarios
         mainmenu = tk.Menu(self.master)
-        mainmenu.add_command(label="Enter input", command=self.input)  # add commands
+        mainmenu.add_command(label="Enter input", command=self._input)
+        mainmenu.add_command(label="Enter obstacles", command=self._obstacles)
         mainmenu.add_command(label="Exit", command=self.master.quit)
         self.master.config(menu=mainmenu)
 
-    def input(self):
+    def _input(self):
+        """
+        Creates new window for entering start/end coordinates and size of robot
+        :return:
+        """
         input_root = tk.Tk()
         input_root.geometry("600x300")
         gui = Input_GUI(input_root)
+        input_root.mainloop()  # from here we can use updated values
+
+    def _obstacles(self):
+        """
+        Creates new window for entering obstacles coordinates
+        :return:
+        """
+        input_root = tk.Tk()
+        input_root.geometry("600x300")
+        gui = Obstacles_GUI(input_root)
         input_root.mainloop()
-        print(gui.start_x)
+
+    def _canvas_init(self):
+        self.canvas = tk.Canvas(self.frame, bg="white", height=500, width=500)
+        self.canvas.pack()
+
+    def _do(self):
+        button = tk.Button(self.r_frame, text="Do", command=self.__do, bg='green', height=30, width=50)
+        button.pack()
+
+    def __do(self):
+        self.connector(size=500)
+
+    def connector(self, size=500):
+        obstacles = load(open('obstacles.json'))
+        coordinates = load(open('input.json'))
+        end_route = shortest_route(coordinates, obstacles, size=size)
+        self.draw_scene(end_route, coordinates, obstacles)
+
+    def draw_circle(self, x, y, r, **kwargs):
+        x = int(x)
+        y = int(y)
+        r = int(r)
+        self.canvas.create_oval(x - r, y - r, x + r, y + r, **kwargs)
+
+    def draw_line(self, x0, y0, x1, y1, **kwargs):
+        self.canvas.create_line(x0, y0, x1, y1, **kwargs)
+
+    def draw_scene(self, route, coordinates, obstacles):
+        self.canvas.delete('all')
+        for x in range(0, self.canvas.winfo_width(), int(coordinates['size'])):
+            self.draw_line(x, 0, x, self.canvas.winfo_height())
+        for y in range(0, self.canvas.winfo_height(), int(coordinates['size'])):
+            self.draw_line(0, y, self.canvas.winfo_width(), y)
+
+        point = [int(coordinates['s_x']), int(coordinates['s_y'])]
+        self.draw_circle(coordinates['s_x'], coordinates['s_y'], coordinates['size'], fill='green')
+        self.draw_circle(coordinates['e_x'], coordinates['e_y'], coordinates['size'], fill='red')
+        for obstacle in obstacles.values():
+            self.draw_circle(obstacle['x'], obstacle['y'], obstacle['r'], fill='black')
+        for node in route[::-1]:
+            self.draw_line(point[0], point[1], node.x, node.y, fill='green', width=3)
+            point[0] = node.x
+            point[1] = node.y
 
 
-def build():
+def main():
     root = tk.Tk()
     root.geometry("600x400")
     gui = GUI(root)
     root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
